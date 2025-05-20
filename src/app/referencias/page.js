7119c0c033ee672cbccb06cb4d410d11737fb198
Everton Ataide <everton.ataide@vtex.com>
2025-05-20 15:49:06 -0300

    Adicionar referencias textuais

--- src/app/referencias/page.js
+++ src/app/referencias/page.js
@@ -10,6 +10,10 @@ export default function Page() {
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
                 <div className="referencesBody">
                     <div className="referencesContent">
+                        <h1>Textuais</h1>
+                        <br />
+
+
                         <p>
                             {'REED, C. How the CIA Taught the Portuguese to Torture. Disponível em: <https://www.counterpunch.org/2004/05/21/how-the-cia-taught-the-portuguese-to-torture/>. Acesso em: 18 maio. 2025.'}
                         </p>
@@ -41,6 +45,33 @@ export default function Page() {
                         <p>
                             {'HENRIQUE, A. Breve história de Portugal. [s.l.] Editorial Presença, 1995.'}
                         </p>
+                        <br />
+                        <h1> Imagens </h1>
+                        <br />
+
+                        <p>{`Retrato Oficial de Salazar do Diretório Geral de Segurança. , 1940. Disponível em: <https://actd.iict.pt/view/actd:AHUD23661>`}</p>
+                        <br />
+                        <p>{`Retrato Oficial de Salazar. , 1968. Disponível em: <https://www.cml.pt/leiloes/2014/158-leilao/1-sessao/384/retrato-de-salazar>`}</p>
+                        <br />
+                        <p>{`Discurso de Salazar no fechamendo da primeira legislatura da Segunda Republica Portuguesa. , 28 abr. 1938. Disponível em: <https://www.dn.pt/edicao-do-dia/27-jul-2020/quando-salazar-o-sabichao-de-coimbra-foi-entrevistado-por-antonio-ferro--12463306.html>`}</p>
+                        <br />
+                        <p>{`Visita da Rainha Elizabeth II a Portugal em 1957. , 19 fev. 1957. Disponível em: <https://arquivomunicipal3.cm-lisboa.pt/X-arqWeb/SearchResultOnline.aspx?search=_OB%3a%2b_QT%3aMFN_1351739_Q%3a_EQ%3aT_D%3aT___&type=PCD&mode=0&page=0&res=1&simple=1&auth=H2gARa5BZQenpjuMYQqqpU%2fni961Jxtbf1uPas4qCyU%3d>`}</p>
+                        <br />
+                        <p>{`Visita de António Olveira Salazar à Madera. , abr. 1925. Disponível em: <https://cultura.madeira.gov.pt/olhares-sobre-o-passado/159-visita-de-ant%C3%B3nio-oliveira-salazar-%C3%A0-madeira.html>`}</p>
+                        <br />
+                        <p>{`Os ministros do novo governo liderado por António de Oliveira Salazar prestam o Compromisso de Honra perante o Presidente Carmona, no Palácio de Belém, a 5 de Julho de 1932. , 5 jul. 1932. Disponível em: < Os ministros do novo governo liderado por António de Oliveira Salazar prestam o Compromisso de Honra perante o Presidente Carmona, no Palácio de Belém, a 5 de Julho de 1932.>`}</p>
+                        <br />
+                        <p>{`Salazar como “Salvador da Patria” no corpo de Afonso I, primeiro rei de Portugal. , 1935. Disponível em: <https://malomil.blogspot.com/2012/03/este-postal-ilustrado-de-1935_23.html>`}</p>
+                        <br />
+                        <p>{`Bandeira do União Nacional. , 2015. Disponível em: <https://curiales.nl/wp-content/uploads/2015/08/logo.png>`}</p>
+                        <br />
+                        <p>{`Visita de Baltazar Rebelo de Sousa à vila de Figueiró dos Vinhos. , 1956. Disponível em: <https://www.flickr.com/photos/bmfigueirodosvinhos/3541997813/>`}</p>
+                        <br />
+                        <p>{`Olveira Salazar e o Nucleo Apostólico, Cardeal Pietro Ciriaci durante a ratificação Concordata de 1940. , 1 jun. 1940. Disponível em: <https://books.openedition.org/cidehus/3579>`}</p>
+                        <br />
+                        <p>{`Antonio de Olivera Salazar sentado em sua mesa de trabalho. , 1940. Disponível em: <https://www.google.com/culturalinstitute/asset-viewer/1wEVz26e74Zcpw>`}</p>
+                        <br />
+
                     </div>
                 </div>
             </main>
